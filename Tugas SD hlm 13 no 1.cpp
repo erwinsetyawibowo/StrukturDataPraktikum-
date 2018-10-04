adc642445e0d086588465542af609cd3e5cab19a
erwinsetyawibowo <43662243+erwinsetyawibowo@users.noreply.github.com>
2018-10-04 16:43:11 +0700

    Add files via upload

---
+++ Tugas SD hlm 13 no 1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+main(){
+
+	int c, b;
+	float rata_rata, tota;
+	
+		cout<<"Masukkan jumlah angkanya = ";cin>>b;
+	int a[b];
+	#include <iostream>
+	for(c=0;c<b;c++){
+		cout<<"Masukkan angka ke-"<<1+c<<" = ";cin>>a[b];
+	tota=tota+a[b];
+	
+	}
+	
+	cout<<endl;
+	rata_rata=tota/b;
+		cout<<"Hasil rata-ratanya = "<<rata_rata;
+	
+getch();
+
+
+}
+	
